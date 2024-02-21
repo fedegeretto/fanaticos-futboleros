23bbbd180d41a3629068a42f032c82fd250e0a36
fedegeretto <fede.geretto96@gmail.com>
2024-02-20 21:00:39 -0300

    modificando el app.js por primera vez

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          FG Programación Web
         </p>
         <a
           className="App-link"
